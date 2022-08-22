035b1d941e105768e9367f807c5c7c092a92c551
ALİ ERKAM İMAM <111283289+alierkamimam@users.noreply.github.com>
2022-08-22 10:30:52 +0300

    Add files via upload

---
+++ ComputeArea.java
@@ -0,0 +1,21 @@
+package weeks.weeks_02;
+
+public class ComputeArea {
+	public static void main (String [] args) {
+		double radius;	//Declare radius
+		double area;	//declare area
+		
+		//Assing a radius
+		radius = 20; //Radius is 20
+		
+		//Compute area
+		
+		area = radius * radius * 3.14159 ;
+		System.out.println("The area for the circle of radius " + 
+		   radius +  " is " + area);
+		
+		
+	}
+	
+
+}
