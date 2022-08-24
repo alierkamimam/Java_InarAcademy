25636ef8be9cb40c478cf5f4750d5ad66939517b
ALİ ERKAM İMAM <111283289+alierkamimam@users.noreply.github.com>
2022-08-24 19:37:15 +0300

    unit 3 checkpoints

---
+++ src/Chapter/checkpoint_03/cp2.java
@@ -0,0 +1,16 @@
+package Chapter.checkpoint_03;
+
+public class cp2 {
+	public static void main(String[] args) {
+		
+		int x = 1;
+		
+		System.out.println(x>0);
+		System.out.println(x<0);
+		System.out.println(x!=0);
+		System.out.println(x>=0);
+		System.out.println(x!=1);
+	}
+	
+
+}
---
+++ src/Chapter/checkpoint_03/cp4.java
@@ -0,0 +1,17 @@
+package Chapter.checkpoint_03;
+import java.util.Scanner;
+public class cp4 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+	    System.out.println("Enter a 'y' value: ");
+	    int y = input.nextInt();
+	    int x;
+	    
+	    if(y >0) x=1;
+	    	System.out.println("x=1");
+		
+		
+	}
+
+}
