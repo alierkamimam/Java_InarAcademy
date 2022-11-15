c3caf3bdd2240e9720536c73fd8fabde518fdc8d
ALİ ERKAM İMAM <imam.ali.erkam@gmail.com>
2022-11-15 10:52:58 +0300

    added

---
+++ src/com.java-inar-academy/src/chapter/chapter11/checkpoint/Circle.java
@@ -0,0 +1,33 @@
+package chapter.chapter11.checkpoint;
+
+public class Circle {
+    private double radius;
+
+         public Circle(double radius) {
+         this.radius = radius;
+         }
+
+         public double getRadius() {
+         return this.radius;
+         }
+
+         public double getArea() {
+         return radius * radius * Math.PI;
+         }
+ }
+
+         class B extends Circle {
+             private double length;
+
+             B(double radius, double length) {
+                 super(radius);
+
+                 this.length = length;
+             }
+
+             @Override
+             public double getArea() {
+                 return super.getArea() * length;
+
+             }
+         }
