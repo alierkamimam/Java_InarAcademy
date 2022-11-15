befe822facfb8c5a409e8b41e279228240a3953e
ALİ ERKAM İMAM <imam.ali.erkam@gmail.com>
2022-11-15 10:41:32 +0300

    added

---
+++ src/com.java-inar-academy/src/chapter/chapter11/exercises/Test.java
@@ -0,0 +1,22 @@
+package chapter.chapter11.exercises;
+
+public class Test {
+    public static void main(String[] args) {
+        A a=new A();
+        a.p(10);
+        a.p(10.0);
+
+    }
+}
+class B{
+    public void p(double i){
+        System.out.println(i * 2);
+    }
+}
+
+class A extends B{
+    @Override
+    public void p(double i) {
+        System.out.println(i);
+    }
+}
