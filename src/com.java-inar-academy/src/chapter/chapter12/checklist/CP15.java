f62ac4e0ec0fba95d3b4c3ade81e297cdf08ef40
ALİ ERKAM İMAM <imam.ali.erkam@gmail.com>
2022-11-21 11:38:13 +0300

    added

---
+++ src/com.java-inar-academy/src/chapter/chapter12/checklist/CP15.java
@@ -0,0 +1,21 @@
+package chapter.chapter12.checklist;
+
+public class CP15 {
+    public static void main(String[] args) {
+        try {
+            method();
+            System.out.println("After the method call");
+        } catch (ArithmeticException ex) {
+            System.out.println("ArithmeticException");
+        } catch (RuntimeException ex) {
+            System.out.println("RuntimeException");
+        } catch (Exception e) {
+            System.out.println("Exception");
+        }
+    }
+
+    static void method() throws Exception {
+
+        System.out.println(1 / 0);
+    }
+}
