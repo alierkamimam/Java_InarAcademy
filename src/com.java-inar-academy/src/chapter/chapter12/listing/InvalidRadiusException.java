649ac1a0df9e8fd54469dcd3be36bfdbd6dd47c3
ALİ ERKAM İMAM <imam.ali.erkam@gmail.com>
2022-11-22 10:37:21 +0300

    added

---
+++ src/com.java-inar-academy/src/chapter/chapter12/listing/InvalidRadiusException.java
@@ -0,0 +1,15 @@
+package chapter.chapter12.listing;
+
+public class InvalidRadiusException extends Exception {
+    private double radius;
+
+
+    public InvalidRadiusException(double radius) {
+        super(" Invalid Radius" + radius);
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+}
