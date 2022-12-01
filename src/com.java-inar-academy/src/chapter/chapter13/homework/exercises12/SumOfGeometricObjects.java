87068a15bfda2bdc79da4da021ee479442aca8b0
ALİ ERKAM İMAM <imam.ali.erkam@gmail.com>
2022-12-01 12:58:49 +0300

    added

---
+++ src/com.java-inar-academy/src/chapter/chapter13/homework/exercises12/SumOfGeometricObjects.java
@@ -0,0 +1,22 @@
+package chapter.chapter13.homework.exercises12;
+
+import chapter.chapter13.homework.exercises01.Triangle;
+import chapter.chapter13.homework.exercises10.GeometricObjects;
+import chapter.chapter13.homework.exercises10.Rectangle;
+import chapter.chapter13.homework.exercises11.Octagon;
+
+public class SumOfGeometricObjects {
+    public static void main(String[] args) {
+        GeometricObjects[] array={new Rectangle(2,9),new Rectangle(3,6),new Octagon(7)};
+        System.out.printf("Sum the areas of geometric objects: %2.2f ",sumArea(array));
+
+
+    }
+    public static double sumArea(GeometricObjects[] a){
+        double sum=0;
+        for (int i = 0; i < a.length; i++) {
+            sum+=a[i].getArea();
+        }
+        return sum;
+    }
+}
