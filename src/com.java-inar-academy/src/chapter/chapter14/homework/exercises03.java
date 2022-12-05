f97d2e1abb50a870374a9501b97ee6abb43bdd05
ALİ ERKAM İMAM <imam.ali.erkam@gmail.com>
2022-12-05 09:56:25 +0300

    added

---
+++ src/com.java-inar-academy/src/chapter/chapter14/homework/exercises03.java
@@ -0,0 +1,34 @@
+package chapter.chapter14.homework;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class exercises03 {
+    public static void main(String[] args) {
+        List<Integer> list = generateRandomNumber();
+        System.out.println(list);
+        List<Integer> reverse=reverse(list);
+        System.out.println(reverse);
+
+
+
+    }
+
+    private static  List<Integer> reverse(List<Integer> list) {
+        List<Integer> reverse=new ArrayList<>();
+        for (int i = list.size()-1; i >=0; i--) {
+            reverse.add(list.get(i));
+
+
+        }
+        return reverse;
+    }
+
+    private static List<Integer> generateRandomNumber() {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            list.add((int) (Math.random() * 10));
+        }
+        return list;
+    }
+}
